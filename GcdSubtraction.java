64965e6fd553644e6989b12550e346463d33cb4c
Robert-ComputerScience <robert29578@gmail.com>
2024-03-07 09:37:24 -0500

    Done!!

--- GcdSubtraction.java
+++ GcdSubtraction.java
@@ -3,17 +3,19 @@ public class GcdSubtraction
 
 
  // I modified it so It can subtract instead of using the modulus operator
-    public static int gcdSubtraction(int x, int y) {
-        if (x == y) // base case
-            return x;
-        else if (x > y)
-            return gcdSubtraction(x - y, y);
-        else
-            return gcdSubtraction(x, y - x);
-    }
+ // Using an else if to subtract it both ways
+ public static int gcd(int x, int y) {
+     if (x < y) {
+         return gcd(y, x); // swap x and y if x is less than y
+     } else if (y == 0) {
+         return x; // gcd is x when y is 0
+     } else {
+         return gcd(y, x - y); // recursive call
+     }
+ }
  // Prints The Subtracted GCD
     public static void main(String[] args) {
-        int rS = gcdSubtraction(118, 448);
+        int rS = gcd(48, 18);
         System.out.println("GCD Subtraction: " + rS);
     }
 }
